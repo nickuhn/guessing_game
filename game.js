9b68fcfadae9bb91969913b08a415bf1840e53d2
Nick Kuhn <nickuhn@gmail.comn>
2015-05-15 15:06:48 -0700

    Refactored entire project
    The html page now has multiple boxes to input duration guesses.
    The name and birthday are also input on index.html
    Instead of looping the JS evaluates answers and provides feedback on each submit button click.

--- game.js
+++ game.js
@@ -1,20 +1,14 @@
 
 (function(){
   //Variable Declarations
-
-  var message, userGuess, correctBreak, user, game;
-  var timesGuessed   = 0;
-  var quit           = 0;
-  var quitting       = 0;
+  var user, daysGuess, hoursGuess, minutesGuess, secondsGuess, message;
   var today          = new Date();
   var todayMs        = today.getTime();
 
-  //Object Declarations:
-
+  //Player object constructor:
  function Player() {
-    this.initialize = function(){
-      this.name         = prompt("What is your name?");
-      this.birthInput   = prompt("what is your birthday? If unsure about time just put in 12:00:", "mm/dd/year hh:mm").split(/\W/);
+    this.initialize     = function(){
+      this.birthInput   = document.getElementById('birth-input').value.split(/\W/);
       this.birthdate    = new Date(this.birthInput[2], this.birthInput[0]-1, this.birthInput[1], this.birthInput[3], this.birthInput[4]);
       this.birthMs      = this.birthdate.getTime();
       this.aliveMs      = todayMs - this.birthMs;
@@ -23,69 +17,52 @@
       this.aliveHours   = Math.floor(this.aliveMinutes/60);
       this.aliveDays    = Math.floor(this.aliveHours/24);
     }
-  }
-
-  function Game() {
-    this.timeIncrements = ["days","hours","minutes","seconds"];
-    this.durationsAlive = [user.aliveDays, user.aliveHours, user.aliveMinutes, user.aliveSeconds];
-    this.counterMax     = this.durationsAlive.length;
-    this.counter        = 0;
-    /*call the guessing function to ask user to guess, seconds, minutes, hours,
-    and days alive.*/
-    this.play = function() {
-      while (quitting != 1 && this.counter < this.counterMax) {
-        quizLoop(this.timeIncrements[this.counter] , this.durationsAlive[this.counter]);
-        this.counter ++;
-      }
+    this.show = function(){
+      document.getElementById('player-name').textContent = document.getElementById('name-input').value;
+      document.getElementById('birthdate').textContent = this.birthdate.toDateString();
     }
   }
 
-  /*****Main function to guess the duration. timeString fills in duration units;
-   hours days etc. Runs through loop prompting and incrementing the timesGuessed
-   until the correct answer is given or the user inputs "quit".*****/
-  quizLoop = function(timeString, duration) {
-    userGuess = prompt(user.name + " you were born on " + user.birthdate + ". How many " + timeString + " do you think you have been alive? Guess within 10% to be correct! Type quit to quit the game.");
+  //Function to determine if the users guess is correct, if not returns higher or lower guidance
+  var guessEval = function(userGuess, duration) {
     if (duration * 0.9 < userGuess && userGuess < duration * 1.1) {
-    message = "Congrats that is correct";
+      message = "That is correct!"
+    } else if (userGuess < duration) {
+      message = "Think bigger!"
     } else {
-      correctBreak = 0;
-      while (userGuess != "quit" && correctBreak != 1) {
-        if( duration * 0.9 < userGuess && userGuess < duration * 1.1 ) {
-          correctBreak = 1;
-        } else {
-          if (userGuess < duration) {
-            message = "Try again, but think bigger";
-          } else {
-            message = "Try again, but not so high";
-          }
-          alert(message);
-          timesGuessed += 1
-          userGuess = prompt(user.name + " you were born on " + user.birthdate + ". How many " + timeString + " do you think you have been alive? Guess within 10% to be correct! Type quit to quit the game.");
-        }
-      }
-      if (userGuess == "quit") {
-        message = "Quitting already?";
-        timesGuessed -= 1;
-        quitting = 1;
-      } else {
-        message = "Congrats that is correct!";
-        quitting = 0;
-      }
+      message = "Think smaller!"
     }
-    //increment the times guessed and print message to screen.
-    timesGuessed ++;
-    alert(message);
+    return message;
   }
 
-  //Initialize Player
+  //Creates a player object on mouse click and populates player info section
+  document.getElementById('start-button').onclick = function() {
   user = new Player();
   user.initialize();
+  user.show();
+  }
 
-  //create new game and play
-  game = new Game();
-  game.play();
+  //Buttons to submit player answer and then evaluate the guesses.
+  document.getElementById('submit-days-button').onclick = function() {
+  daysGuess = document.getElementById('guess-days').value;
+  document.getElementById('days-result').textContent = guessEval(daysGuess, user.aliveDays);
+  }
 
-  //let the user know how many tries it took them to find the answer.
-  alert("It took you " + timesGuessed + " guesses to find the answers.");
+  document.getElementById('submit-hours-button').onclick = function() {
+  hoursGuess = document.getElementById('guess-hours').value;
+  document.getElementById('hours-result').textContent = guessEval(hoursGuess, user.aliveHours);
+  }
+
+  document.getElementById('submit-minutes-button').onclick = function() {
+  minutesGuess = document.getElementById('guess-minutes').value;
+  document.getElementById('minutes-result').textContent = guessEval(minutesGuess, user.aliveMinutes);
+  }
+
+  document.getElementById('submit-seconds-button').onclick = function() {
+  secondsGuess = document.getElementById('guess-seconds').value;
+  document.getElementById('seconds-result').textContent = guessEval(secondsGuess, user.aliveSeconds);
+  }
+  //Keeps date on site current
+  document.getElementById('current-date').textContent = today.toDateString();
 })();
 
